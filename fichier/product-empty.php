a5380567fc7a49c4401dd5cb40c83acfacc27b92
exau2022 <adjayayassirath2001@gmail.com>
2022-12-14 23:11:58 +0100

    formulaire d'ajout de produit

--- fichier/product-empty.php
+++ fichier/product-empty.php
@@ -1,3 +1,226 @@
 <?php require './header-admin.php'; ?>
+    <div class="col-9" style="padding-left:0">
+        <main style="padding-top:20px"> 
+            <div class="d-flex justify-content-between">
+                <div>
+                    
+                    <button class="btn btn-light text-dark border-light" type="submit">Annuler la modification</button>
+                  
+                </div>
+                
+                <div>
+                    <button type="button" class="btn btn-primary "  data-toggle="modal" data-target="#myModal">
+                    <i class="bi bi-plus"></i>
+                     Valider et enregistrer la fiche
+                    </button>
+                </div>     
+            </div>
+            <form action="">
+                <fieldset class="bg-secondary">
+                    <legend>Gestion</legend>
+                    <div class="form-group row">
+                        <legend class="col-form-label col-sm-2 float-sm-left pt-0">Statut</legend>
+                        <div class="col-sm-10">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                                <label class="form-check-label" for="gridRadios1">
+                                    En attente de mise en ligne
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                                <label class="form-check-label text-success" for="gridRadios2">
+                                  En boutique
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
+                                <label class="form-check-label text-danger" for="gridRadios3">
+                                    Retiré
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="shop" class="col-sm-2 col-form-label">Boutique</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" id="shop">
+                                <option>Périphériques & composants</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="ray" class="col-sm-2 col-form-label">Rayon</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" id="ray">
+                                <option>Ecrans d'ordinateur</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <legend>Présentation</legend>
+                    <div class="form-group row">
+                        <label for="brand" class="col-sm-2 col-form-label">Marque</label>
+                        <div class="col-sm-10">
+                        <input type="text" class="form-control" id="brand">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="reference" class="col-sm-2 col-form-label">Référence</label>
+                        <div class="col-sm-10">
+                        <input type="text" class="form-control" id="reference">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="tag" class="col-sm-2 col-form-label">Etiquettes</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="tag">
+                        </div>
+                    </div>
 
+                    <div class="form-group row">
+                        <label for="tag" class="col-sm-2 col-form-label">Prix</label>
+                        <div class="col-sm-5">
+                            <label class="sr-only" for="price"></label>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">Prix</div>
+                                </div>
+                                <input type="text" class="form-control" id="price" placeholder="109,94">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">£</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-5">
+                            <label class="sr-only" for="inlineFormInputGroup"></label>
+                            <div class="input-group mb-4">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">éco-Participation</div>
+                                </div>
+                                <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="1,70">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">£</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-sm-2 col-form-label">Description principal (MarkDown)</label>
+                        <div class="col-sm-10">
+                         <textarea name="" id="" class="form-control" rows="10"></textarea>
+                        </div>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <legend>Photos</legend>
+                   <div class="form-group row">
+                        <label for="reference" class="col-sm-2 col-form-label">Photo 1</label>
+                        <div class="custom-file col-sm-10">
+                            <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                            <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+                        </div>
+                   </div>
+                   <div class="form-group row">
+                        <label for="reference" class="col-sm-2 col-form-label">Photo 2</label>
+                        <div class="custom-file col-sm-10">
+                            <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                            <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+                        </div>
+                   </div>
+                   <div class="form-group row">
+                        <label for="reference" class="col-sm-2 col-form-label">Photo 3</label>
+                        <div class="custom-file col-sm-10">
+                            <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                            <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+                        </div>
+                   </div>
+                   <div class="form-group row">
+                        <label for="reference" class="col-sm-2 col-form-label">Photo 4</label>
+                        <div class="custom-file col-sm-10">
+                            <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                            <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+                        </div>
+                   </div>
+                   <div class="form-group row">
+                        <label for="reference" class="col-sm-2 col-form-label">Photo 5</label>
+                        <div class="custom-file col-sm-10">
+                            <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                            <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+                        </div>
+                   </div>
+                </fieldset>
+                <fieldset>
+                    <legend>Descriptif 1</legend>
+                    <div class="form-group row">
+                        <label for="tag" class="col-sm-2 col-form-label">Titre</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="tag">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="reference" class="col-sm-2 col-form-label">Image</label>
+                        <div class="custom-file col-sm-10">
+                            <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                            <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+                        </div>
+                   </div>
+                   <div class="form-group row">
+                        <label for="" class="col-sm-2 col-form-label">Texte</label>
+                        <div class="col-sm-10">
+                         <textarea name="" id="" class="form-control" rows="10"></textarea>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Descriptif 2</legend>
+                    <div class="form-group row">
+                        <label for="tag" class="col-sm-2 col-form-label">Titre</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="tag">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="reference" class="col-sm-2 col-form-label">Image</label>
+                        <div class="custom-file col-sm-10">
+                            <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                            <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+                        </div>
+                   </div>
+                   <div class="form-group row">
+                        <label for="" class="col-sm-2 col-form-label">Texte</label>
+                        <div class="col-sm-10">
+                         <textarea name="" id="" class="form-control" rows="10"></textarea>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Descriptif 3</legend>
+                    <div class="form-group row">
+                        <label for="tag" class="col-sm-2 col-form-label">Titre</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="tag">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="reference" class="col-sm-2 col-form-label">Image</label>
+                        <div class="custom-file col-sm-10">
+                            <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                            <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+                        </div>
+                   </div>
+                   <div class="form-group row">
+                        <label for="" class="col-sm-2 col-form-label">Texte</label>
+                        <div class="col-sm-10">
+                         <textarea name="" id="" class="form-control" rows="10"></textarea>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>   
+        </main>
+    </div>
+</div>
 <?php require './footer-admin.php'; ?>
